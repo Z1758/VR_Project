7b49d1c2bf6062abe9eb1f8cd62a65f9ccdd361e
Z1758 <timeofwitch@gmail.com>
2024-10-17 16:32:59 +0900

    사거리 조절 구현

--- Assets/Scripts/BirdController.cs
+++ Assets/Scripts/BirdController.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class BirdController : MonoBehaviour
 {
     [SerializeField] Transform target;
     [SerializeField] LayerMask mask;
+    [SerializeField] Transform wall;
+    [SerializeField] WaitForSeconds wfs = new WaitForSeconds (0.07f);
+
+    [SerializeField] float maxRange;
+    [SerializeField] float minRange;
+    Coroutine co;
+
     public void MoveTarget()
     {
       
@@ -22,6 +30,56 @@ public class BirdController : MonoBehaviour
     private void Update()
     {
         MoveTarget();
-        Debug.DrawRay(transform.position, transform.forward * 20f, Color.red);
+    
+    }
+
+
+    public void FrontBack(InputAction.CallbackContext value)
+    {
+        if (value.started)
+        {
+            if (value.ReadValue<Vector2>().y > 0.1f)
+            {
+                StopAllCoroutines();
+                co = StartCoroutine(BackCoroutine());
+
+
+            }
+            else if (value.ReadValue<Vector2>().y < -0.1f)
+            {
+                StopAllCoroutines();
+                co = StartCoroutine(FrontCoroutine());
+            }
+
+        }
+
+        if (value.canceled)
+        {
+            StopAllCoroutines();
+        }
+
+     
+    }
+
+    IEnumerator FrontCoroutine()
+    {
+        while (true)
+        {
+            if(wall.position.z < maxRange )
+            wall.position = wall.position + Vector3.forward;
+            yield return wfs;
+           
+        }
+    }
+
+    IEnumerator BackCoroutine()
+    {
+        while (true)
+        {
+            if (wall.position.z > minRange)
+                wall.position = wall.position - Vector3.forward;
+            yield return wfs;
+            
+        }
     }
 }
